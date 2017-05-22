eb67384ccd5ee0064c87c2d97976438b67363cf0
Michael Axelson <axelmichael11@gmail.com>
2017-05-22 14:33:43 -0700

    yup were good to go

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,7 +5,10 @@ var app = app || {};
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  aboutController.init = function() {
+    console.log('home view');
+    $('#content').text('Hello from the about page');
+  }
 
   module.aboutController = aboutController;
 })(app);
